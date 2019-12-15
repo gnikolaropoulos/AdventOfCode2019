56cdadd6ee2f1a251fc6d977825cc1b94148460f
George Nikolaropoulos <g.nikolaropoulos@gmail.com>
2019-12-15 21:20:06 +0200

    fix day13

--- day13/main.go
+++ day13/main.go
@@ -53,13 +53,13 @@ func countBlocks(program []int64) (count int) {
 		switch message.Kind {
 		case MessageOutput:
 			var pos Vector2
-			pos.x = int(message.Value)
+			pos.X = int(message.Value)
 
 			message = <-messages
 			if message.Kind != MessageOutput {
 				panic("unexpected message")
 			}
-			pos.y = int(message.Value)
+			pos.Y = int(message.Value)
 
 			message = <-messages
 			if message.Kind != MessageOutput {
@@ -104,8 +104,8 @@ func emulateArcadeCabinet(program []int64) int64 {
 					max = max.Max(pos)
 				}
 
-				for y := min.y; y <= max.y; y++ {
-					for x := min.x; x <= max.x; x++ {
+				for y := min.Y; y <= max.Y; y++ {
+					for x := min.X; x <= max.X; x++ {
 						switch grid[Vector2{x, y}] {
 						case Empty:
 							fmt.Print(" ")
@@ -136,23 +136,23 @@ func emulateArcadeCabinet(program []int64) int64 {
 					paddle = pos
 				}
 			}
-			input <- int64(sign(ball.x - paddle.x))
+			input <- int64(sign(ball.X - paddle.X))
 
 		case MessageOutput:
 			var pos Vector2
-			pos.x = int(message.Value)
+			pos.X = int(message.Value)
 
 			message = <-messages
 			if message.Kind != MessageOutput {
 				panic("unexpected message")
 			}
-			pos.y = int(message.Value)
+			pos.Y = int(message.Value)
 
 			message = <-messages
 			if message.Kind != MessageOutput {
 				panic("unexpected message")
 			}
-			if pos.x == -1 && pos.y == 0 {
+			if pos.X == -1 && pos.Y == 0 {
 				score = message.Value
 			} else {
 				grid[pos] = message.Value
@@ -167,13 +167,6 @@ func emulateArcadeCabinet(program []int64) int64 {
 	}
 }
 
-// This version of the intcode emulator is synchronous, i.e. it sends a
-// MessageWaitingForInput before reading from the input channel. This makes it
-// possible to base the input on the previous output, without requiring the
-// controlling code to know the exact behavior of the intcode program. Using
-// channels in this way allows for a clean separation between the intcode
-// emulator and the puzzle specific controlling code.
-
 const (
 	MessageWaitingForInput = iota
 	MessageOutput
@@ -186,147 +179,105 @@ type Message struct {
 }
 
 func emulate(program []int64, input <-chan int64, messages chan<- Message) {
-	// Copy the program into memory, so that we do not modify the original.
-	memory := make([]int64, len(program))
+	memory := make([]int64, 3000)
 	copy(memory, program)
 
-	getMemoryPointer := func(index int64) *int64 {
-		// Grow memory, if index is out of range.
-		for int64(len(memory)) <= index {
-			memory = append(memory, 0)
-		}
-		return &memory[index]
-	}
-
 	var ip, relativeBase int64
+
 	for {
 		instruction := memory[ip]
+		a := instruction / 10000
+		b := (instruction - a*10000) / 1000
+		c := (instruction - a*10000 - b*1000) / 100
 		opcode := instruction % 100
 
-		getParameter := func(offset int64) *int64 {
-			parameter := memory[ip+offset]
-			mode := instruction / pow(10, offset+1) % 10
-			switch mode {
-			case 0: // position mode
-				return getMemoryPointer(parameter)
-			case 1: // immediate mode
-				return &parameter
-			case 2: // relative mode
-				return getMemoryPointer(relativeBase + parameter)
-			default:
-				panic(fmt.Sprintf("fault: invalid parameter mode: ip=%d instruction=%d offset=%d mode=%d", ip, instruction, offset, mode))
-			}
-		}
-
 		switch opcode {
-
 		case 1: // ADD
-			a, b, c := getParameter(1), getParameter(2), getParameter(3)
-			*c = *a + *b
+			x := fetchPointerToMemory(c, &memory, ip+1, relativeBase)
+			y := fetchPointerToMemory(b, &memory, ip+2, relativeBase)
+			z := fetchPointerToMemory(a, &memory, ip+3, relativeBase)
+			*z = *x + *y
 			ip += 4
 
 		case 2: // MULTIPLY
-			a, b, c := getParameter(1), getParameter(2), getParameter(3)
-			*c = *a * *b
+			x := fetchPointerToMemory(c, &memory, ip+1, relativeBase)
+			y := fetchPointerToMemory(b, &memory, ip+2, relativeBase)
+			z := fetchPointerToMemory(a, &memory, ip+3, relativeBase)
+			*z = *x * *y
 			ip += 4
 
 		case 3: // INPUT
 			messages <- Message{Kind: MessageWaitingForInput}
-			a := getParameter(1)
-			*a = <-input
+			x := fetchPointerToMemory(c, &memory, ip+1, relativeBase)
+			*x = <-input
 			ip += 2
 
 		case 4: // OUTPUT
-			a := getParameter(1)
-			messages <- Message{Kind: MessageOutput, Value: *a}
+			x := fetchPointerToMemory(c, &memory, ip+1, relativeBase)
+			messages <- Message{Kind: MessageOutput, Value: *x}
 			ip += 2
 
 		case 5: // JUMP IF TRUE
-			a, b := getParameter(1), getParameter(2)
-			if *a != 0 {
-				ip = *b
+			x := fetchPointerToMemory(c, &memory, ip+1, relativeBase)
+			y := fetchPointerToMemory(b, &memory, ip+2, relativeBase)
+			if *x != 0 {
+				ip = *y
 			} else {
 				ip += 3
 			}
 
 		case 6: // JUMP IF FALSE
-			a, b := getParameter(1), getParameter(2)
-			if *a == 0 {
-				ip = *b
+			x := fetchPointerToMemory(c, &memory, ip+1, relativeBase)
+			y := fetchPointerToMemory(b, &memory, ip+2, relativeBase)
+			if *x == 0 {
+				ip = *y
 			} else {
 				ip += 3
 			}
 
 		case 7: // LESS THAN
-			a, b, c := getParameter(1), getParameter(2), getParameter(3)
-			if *a < *b {
-				*c = 1
+			x := fetchPointerToMemory(c, &memory, ip+1, relativeBase)
+			y := fetchPointerToMemory(b, &memory, ip+2, relativeBase)
+			z := fetchPointerToMemory(a, &memory, ip+3, relativeBase)
+
+			if *x < *y {
+				*z = 1
 			} else {
-				*c = 0
+				*z = 0
 			}
+
 			ip += 4
 
 		case 8: // EQUAL
-			a, b, c := getParameter(1), getParameter(2), getParameter(3)
-			if *a == *b {
-				*c = 1
+			x := fetchPointerToMemory(c, &memory, ip+1, relativeBase)
+			y := fetchPointerToMemory(b, &memory, ip+2, relativeBase)
+			z := fetchPointerToMemory(a, &memory, ip+3, relativeBase)
+			if *x == *y {
+				*z = 1
 			} else {
-				*c = 0
+				*z = 0
 			}
+
 			ip += 4
 
-		case 9: // RELATIVE BASE OFFSET
-			a := getParameter(1)
-			relativeBase += *a
+		case 9: // ADJUST RELATIVE BASE
+			x := fetchPointerToMemory(c, &memory, ip+1, relativeBase)
+			relativeBase += *x
 			ip += 2
 
 		case 99: // HALT
 			messages <- Message{Kind: MessageHalt}
 			return
-
 		default:
-			panic(fmt.Sprintf("fault: invalid opcode: ip=%d instruction=%d opcode=%d", ip, instruction, opcode))
+			panic(fmt.Sprintf("error: invalid opcode: ip=%d, instruction=%d, opcode=%d", ip, instruction, opcode))
 		}
 	}
-}
-
-// Integer power: compute a**b using binary powering algorithm
-// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
-// Source: https://groups.google.com/d/msg/golang-nuts/PnLnr4bc9Wo/z9ZGv2DYxXoJ
-func pow(a, b int64) int64 {
-	var p int64 = 1
-	for b > 0 {
-		if b&1 != 0 {
-			p *= a
-		}
-		b >>= 1
-		a *= a
-	}
-	return p
 }
 
 func toInt64(s string) int64 {
 	result, err := strconv.ParseInt(s, 10, 64)
 	check(err)
 	return result
-}
-
-type Vector2 struct {
-	x, y int
-}
-
-func (v Vector2) Min(other Vector2) Vector2 {
-	return Vector2{
-		x: min(v.x, other.x),
-		y: min(v.y, other.y),
-	}
-}
-
-func (v Vector2) Max(other Vector2) Vector2 {
-	return Vector2{
-		x: max(v.x, other.x),
-		y: max(v.y, other.y),
-	}
 }
 
 func readFile(filename string) string {
@@ -341,14 +292,40 @@ func check(err error) {
 	}
 }
 
-func sign(x int) int {
-	if x > 0 {
-		return 1
+type Vector2 struct {
+	X, Y int
+}
+
+// Sub returns the standard vector difference of v and ov (other vector).
+func (v Vector2) Sub(ov Vector2) Vector2 {
+	return Vector2{
+		v.X - ov.X,
+		v.Y - ov.Y,
 	}
-	if x < 0 {
-		return -1
+}
+
+// Sub returns the standard vector sum of v and ov (other vector).
+func (v Vector2) Add(ov Vector2) Vector2 {
+	return Vector2{
+		v.X + ov.X,
+		v.Y + ov.Y,
 	}
-	return 0
+}
+
+// Min returns a Vector2 with the minimum coordiates of v and ov (other vector).
+func (v Vector2) Min(ov Vector2) Vector2 {
+	return Vector2{
+		min(v.X, ov.X),
+		min(v.Y, ov.Y),
+	}
+}
+
+// Max returns a Vector2 with the maximum coordiates of v and ov (other vector).
+func (v Vector2) Max(ov Vector2) Vector2 {
+	return Vector2{
+		max(v.X, ov.X),
+		max(v.Y, ov.Y),
+	}
 }
 
 func min(x, y int) int {
@@ -363,4 +340,32 @@ func max(x, y int) int {
 		return y
 	}
 	return x
+}
+
+func sign(x int) int {
+	if x > 0 {
+		return 1
+	}
+
+	if x < 0 {
+		return -1
+	}
+
+	return 0
+}
+
+func fetchPointerToMemory(mode int64, memory *[]int64, position int64, relativeBase int64) *int64 {
+
+	switch mode {
+	case 0:
+		index := (*memory)[position]
+		return &(*memory)[index]
+	case 1:
+		return &(*memory)[position]
+	case 2:
+		index := (*memory)[position] + relativeBase
+		return &(*memory)[index]
+	default:
+		panic("error in mode")
+	}
 }
