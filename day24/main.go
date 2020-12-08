59423bda1a7cea25c238f5cd85fed00c17c6c90a
George <g.nikolaropoulos@gmail.com>
2020-12-08 17:08:39 +0200

    add missing part of day 24

--- day24/main.go
+++ day24/main.go
@@ -76,6 +76,125 @@ func main() {
 
 	fmt.Println("--- Part One ---")
 	fmt.Println(rating)
+
+	{
+		fmt.Println("--- Part Two ---")
+
+		var grid [5][5]bool
+		for y, line := range lines {
+			for x, char := range line {
+				if char == '#' {
+					grid[y][x] = true
+				}
+			}
+		}
+
+		state := make(map[int][5][5]bool)
+		state[0] = grid
+		min, max := 0, 0
+
+		for minute := 0; minute < 200; minute++ {
+			next := make(map[int][5][5]bool)
+
+			for index := min - 1; index <= max+1; index++ {
+				var nextGrid [5][5]bool
+
+				for y := 0; y < 5; y++ {
+					for x := 0; x < 5; x++ {
+						if x == 2 && y == 2 {
+							continue
+						}
+
+						neighbors := 0
+
+						if x > 0 && state[index][y][x-1] {
+							neighbors++
+						}
+						if x < 4 && state[index][y][x+1] {
+							neighbors++
+						}
+						if y > 0 && state[index][y-1][x] {
+							neighbors++
+						}
+						if y < 4 && state[index][y+1][x] {
+							neighbors++
+						}
+
+						if x == 0 && state[index-1][2][1] {
+							neighbors++
+						}
+						if x == 4 && state[index-1][2][3] {
+							neighbors++
+						}
+						if y == 0 && state[index-1][1][2] {
+							neighbors++
+						}
+						if y == 4 && state[index-1][3][2] {
+							neighbors++
+						}
+
+						if x == 1 && y == 2 {
+							for i := 0; i < 5; i++ {
+								if state[index+1][i][0] {
+									neighbors++
+								}
+							}
+						}
+
+						if x == 3 && y == 2 {
+							for i := 0; i < 5; i++ {
+								if state[index+1][i][4] {
+									neighbors++
+								}
+							}
+						}
+
+						if y == 1 && x == 2 {
+							for i := 0; i < 5; i++ {
+								if state[index+1][0][i] {
+									neighbors++
+								}
+							}
+						}
+
+						if y == 3 && x == 2 {
+							for i := 0; i < 5; i++ {
+								if state[index+1][4][i] {
+									neighbors++
+								}
+							}
+						}
+
+						nextGrid[y][x] = (state[index][y][x] && neighbors == 1) || (!state[index][y][x] && neighbors >= 1 && neighbors <= 2)
+					}
+				}
+
+				next[index] = nextGrid
+			}
+
+			state = next
+			min, max = min-1, max+1
+		}
+
+		bugs := 0
+		for _, layer := range state {
+			bugs += count(layer)
+		}
+
+		fmt.Println(bugs)
+	}
+}
+
+func count(grid [5][5]bool) (bugs int) {
+	for y := 0; y < 5; y++ {
+		for x := 0; x < 5; x++ {
+			if grid[y][x] {
+				bugs++
+			}
+		}
+	}
+
+	return
 }
 
 func readLines(filename string) []string {
